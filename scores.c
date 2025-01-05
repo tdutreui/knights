76ca54ff026a5dfada48d77b1a880eac84bb8e6a
tdutreui <thomas.dutreuilh@gmail.com>
2025-01-05 19:38:10 +0100

    sorted High scores

--- scores.c
+++ scores.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,21 +10,21 @@ struct Element
 	Element *suivant;
 };
 
-typedef struct Pile Pile;
-struct Pile
+typedef struct File File;
+struct File
 {
 	Element *premier;
 };
 
-void afficherPile(Pile *pile)
+void display(File *file)
 {
-	if (pile == NULL)
+	if (file == NULL)
 	{
 		exit(EXIT_FAILURE);
 	}
-	Element *actuel = pile->premier;
+	Element *actuel = file->premier;
 
-    printf("High scores :\n");
+	printf("High scores :\n");
 	while (actuel != NULL)
 	{
 		printf("%s\t", actuel->name);
@@ -43,28 +35,76 @@ void afficherPile(Pile *pile)
 	printf("\n");
 }
 
-void empiler(Pile *pile, char* name, int value)
+
+void add(File *file, char* name, int value)
 {
 	Element *nouveau = malloc(sizeof(*nouveau));
-	if (pile == NULL || nouveau == NULL)
+	if (file == NULL || nouveau == NULL)
 	{
 		exit(EXIT_FAILURE);
 	}
 
 	nouveau->name = name;
 	nouveau->value = value;
-	nouveau->suivant = pile->premier;
-	pile->premier = nouveau;
+	nouveau->suivant = file->premier;
+	file->premier = nouveau;
 }
 
-
-Element* findByName(Pile *pile, char* name)
+void sortedAdd(File *file, char* name, int value)
 {
-	if (pile == NULL)
+	Element *nouveau = malloc(sizeof(*nouveau));
+	if (file == NULL || nouveau == NULL)
 	{
 		exit(EXIT_FAILURE);
 	}
-	Element *actuel = pile->premier;
+
+	nouveau->name = name;
+	nouveau->value = value;
+
+
+	Element *actuel = file->premier;
+	
+	//La liste est vide
+	if (actuel==NULL)
+	{
+		nouveau->suivant = NULL;
+		file->premier = nouveau;
+		return;
+	}
+
+	//Insertion ordonnée par score
+	while (actuel != NULL)
+	{
+		if(actuel->value > nouveau->value) //Si l'actuel est supérieur
+		{
+			if(actuel->suivant==NULL || actuel->suivant->value <= nouveau->value) //ET que le suivant de l'actuel est inférieur ou inexistant
+			{
+				nouveau->suivant=actuel->suivant;
+				actuel->suivant=nouveau;
+				return;
+			}
+		}
+		actuel = actuel->suivant;
+
+		if (actuel==NULL)
+		{
+			//On arrive à la fin de la file, aucun score n'est plus élevé que "nouveau"
+			//On place donc "nouveau" au début
+			nouveau->suivant = file->premier;
+			file->premier = nouveau;
+			return;
+		}
+	}
+}
+
+
+Element* findByName(File *file, char* name)
+{
+	if (file == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
+	Element *actuel = file->premier;
 
 	while (actuel != NULL)
 	{
@@ -78,34 +118,68 @@ Element* findByName(Pile *pile, char* name)
 	return NULL;
 }
 
-
-void addOrUpdate(Pile *pile, char* name, int value)
+void addOrUpdate(File *file, char* name, int value)
 {
 	Element* elementExistant;
-	elementExistant=findByName(pile,name);
+	elementExistant=findByName(file,name);
 	if (elementExistant!=NULL) {
 		if(elementExistant->value < value)
 		{
 			elementExistant->value=value;
 		}
 	} else {
-		empiler(pile,name,value);
+		add(file,name,value);
 	}
+}
+
+// Retourne une nouvelle liste identique mais ordonnée par score
+File* trier(File * file)
+{
+	if (file == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
+
+	//nouvelle file qui sera triée
+	File *fileTriee = malloc(sizeof(File));
+	fileTriee->premier=NULL;
+
+	Element *actuel = file->premier;
+	while (actuel != NULL)
+	{
+		sortedAdd(fileTriee,actuel->name,actuel->value);
+		actuel = actuel->suivant;
+	}
+	
+	return fileTriee;
 }
 
 
 int main()
 {
-	Pile *maPile = malloc(sizeof(Pile));
+	File *maFile = malloc(sizeof(File));
+	maFile->premier=NULL;
 
-	addOrUpdate(maPile, "Toto",4);
-	addOrUpdate(maPile,"Toto", 8);
-	addOrUpdate(maPile, "Lulu", 15);
-	addOrUpdate(maPile, "Lulu", 10);
+	//chargement du fichier et élimination des doublons
+	addOrUpdate(maFile, "Toto",4);
+	addOrUpdate(maFile,"Toto", 8);
+	addOrUpdate(maFile, "Wiwi", 30);
+	addOrUpdate(maFile, "Lulu", 15);
+	addOrUpdate(maFile, "Lulu", 10);
 
-	afficherPile(maPile);
+	display(maFile);
 	/*
 	High scores :
+	Lulu    15
+	Wiwi    30
+	Toto    8
+	*/
+
+	maFile=trier(maFile);
+	display(maFile);
+	/*
+	High scores :
+    Wiwi    30
     Lulu    15
     Toto    8
 	*/
